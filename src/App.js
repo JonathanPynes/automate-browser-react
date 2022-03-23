aadd24d1497e9d2c72db84de2bb0b296b456a1f6
Jonathan Pynes <jon.pynes@shineforth.co>
2022-03-23 10:47:28 -0500

    Added ancient disapline

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <OpenTabs />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Choose what you want most, over what you want now.
         </p>
         <a
           className="App-link"
